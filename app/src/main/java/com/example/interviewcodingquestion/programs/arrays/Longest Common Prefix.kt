ddc2895b539fb77b60e3a146e457e9677d90a8d4
Gurvinder Singh <gurvinderscs@gmail.com>
2024-04-29 19:09:03 +0530

    Updated questions

--- app/src/main/java/com/example/interviewcodingquestion/programs/arrays/Longest Common Prefix.kt
+++ app/src/main/java/com/example/interviewcodingquestion/programs/arrays/Longest Common Prefix.kt
@@ -11,7 +11,6 @@ fun main() {
     val result = longestCommonPrefix(arr)
     println(result) // "aa"
 }
-
 /**
  * The time complexity of the provided solution is O(n * m * log(n)),
  * and the space complexity is O(1),
